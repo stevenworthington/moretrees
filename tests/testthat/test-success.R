77a6f61c8fcb8b72abffe935423398e77d2e7127
stevenworthington <sworthington@iq.harvard.edu>
2020-03-12 16:54:14 -0400

    travis

--- tests/testthat/test-success.R
+++ tests/testthat/test-success.R
@@ -5,3 +5,7 @@ test_that("one plus one is two", {
 test_that("you can skip tests if needed", {
   skip("This tests hasn't been written yet")
 })
+
+test_that("you can skip tests if needed", {
+  skip("This tests hasn't been written yet")
+})
